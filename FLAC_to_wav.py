11522b9ff9a24a096d1106f4bcd46b00080b5344
BeckyHeath <r.heath18@imperial.ac.uk>
2021-11-15 14:33:22 +0000

    create and test convert to wav script

---
+++ FLAC_to_wav.py
@@ -0,0 +1,23 @@
+################################################################################################
+#
+# Script to Convert FLAC files to WAV files for use with HARKBird
+# 
+# Becky Heath 2021
+#
+################################################################################################
+
+# Imports
+import os
+import subprocess
+                    #Move To Folder with Audio Files in 
+
+pre_comp_dir = "Data/Field_Recordings/"
+post_comp_dir ="Data/Field_Recordings_wav/"
+
+for wfile in os.listdir(pre_comp_dir):
+    wfile = pre_comp_dir + wfile
+    ofile= wfile.replace(pre_comp_dir, post_comp_dir)
+    ofile = ofile.replace(".flac",".wav")
+    cmd = ('ffmpeg -i {} {}') 
+    subprocess.call(cmd.format(wfile, ofile), shell=True)
+    #os.remove(wfile)
