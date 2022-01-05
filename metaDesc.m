c0deeffca38949d1dd375cb32c8b31f8c310e7af
BeckyHeath <r.heath18@imperial.ac.uk>
2022-01-05 20:08:13 +0000

    cosmetic changes to string outputs

--- metaDesc.m
+++ metaDesc.m
@@ -3,21 +3,21 @@ function data = metaDesc(val1,val2, val3, val4,val5)
     outStr = "";
 
    if val3 > 0 
-       outStr = val3 + " dead strings";
+       outStr = outStr + " " + val3 + " dead";
        if val4 + val5 > 0 
-           outStr = outStr + " and"; 
+           outStr = outStr + " +"; 
        end 
    end 
 
    if val4 > 0 
-       outStr = val4 + " part dead strings";
+       outStr = outStr + " " +  val4 + " part-dead";
        if val5 > 0 
-           outStr = outStr + " and"; 
+           outStr = outStr + " +"; 
        end 
    end 
       
    if val5 > 0 
-       outStr = val5 + " anomolous channels";
+       outStr = outStr + " " + val5 + " irregular";
    end 
 
    if outStr == ""
