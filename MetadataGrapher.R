bdde53a41b2d8507f46c8fc3dad1c48e8bf9f20e
BeckyHeath <r.heath18@imperial.ac.uk>
2022-03-10 15:04:54 +0000

    get data readouts

--- MetadataGrapher.R
+++ MetadataGrapher.R
@@ -45,7 +45,25 @@ long <- melt(setDT(recStatus), id.vars = c("date"), variable.name = "Recorder")
 
 
 # Join the dataframes
-newFile <- full_join(long,boxMeta, by = c("date","Recorder"))
+combinedDF <- full_join(long,boxMeta, by = c("date","Recorder"))
+
+##### Get data about the recordings #####
+
+fieldDep <- c("conifer", "oak")
+
+justFieldData <- combinedDF[combinedDF$value == fieldDep]
+
+numHours = (sum(justFieldData$numFiles,na.rm = TRUE)*10)/60 # TEN MINUTE RECORDINGS 
+numHours = round(as.numeric(numHours), digits = 2) # 2 dp 
+
+dataVol = (sum(justFieldData$TotFileSizeMB, na.rm = TRUE))/1000
+
+
+printOut = cat("###############################", "\n", "Num Hours: ", numHours, "\n", "Data Volume: ", dataVol, "\n", "######################")
+
+
+##### Graphing Data #####
+
 
 # Seperate out by recorder 
 
