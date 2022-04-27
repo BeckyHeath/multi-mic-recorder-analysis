5a7d232095d646f5bd493aefb2abc661601fa972
BeckyHeath <r.heath18@imperial.ac.uk>
2022-04-27 18:38:44 +0100

    generate big spectral quality figutre

--- spectraDevComparisonrerejigm.m
+++ spectraDevComparisonrerejigm.m
@@ -13,61 +13,83 @@ cd(fileparts(tmp.Filename));
 
 % One at a time
 
+root = "Data/";
+
 RecNames = ["yellow","yellowgreen", "Blue", "green"];
-
-figureName = ["Figures/MicQualData/mega.png"];
-
+SubFolders = ["pre","early"];
 
 % Play with these Order: Yellow, YellowGreen, Blue, Green
 colours = ["F6BD60","86CB92","12664F","00A7E1"];
 colours2 = ["EAC435","18FF6D", "5C9EAD", "415D43"];
 
+% Note: The orders wierd but this is for graphing! 
+
+tok=-6; % this is to organise subfigs
+% Iterate first through recorder types! 
 for k = 1:size(RecNames,2)
-    rootPath = rootPaths(k);
-    figName = figureNames(k);
-
-    Files = dir(rootPath + "*.wav");
-    FileNames =  { Files.name };
-    
-    %figure('WindowState','maximized')
-    
-    for fileNo = 1:size(FileNames,2)
-        i_file = rootPath + FileNames(fileNo);
+    recName = RecNames(k);
+ 
+    % Then go through pre and early:
+    for sub = 1:size(SubFolders,2)
+        folder = SubFolders(sub);
         
-        %Load in Audio
-        aud = audioread(i_file,[1,600*16000]); % Load in full file 
-        
-        % Work out spectra minutewise
-        for endSamp = 960000:4800000:9600000
-            startSamp = 1;
-            samples = [startSamp,endSamp];
-            
-            TestSection = aud(startSamp:endSamp,:);
-            startSamp = endSamp; % Swap over the start/end point for next loop
-            
-            % Iterate through channels
-            j=0;
-            for ch = 1:6
-                j=j+1;
-                audCh = aud(:,ch);
-                [p,frequencies] = pspectrum(audCh,16000);
-                pdb = cat(2,frequencies,pow2db(p));
+        path = root + recName + "/" + folder + "/";
 
-                %ylab = "ch " +  j;
+        Files = dir(path + "*.wav");
+        FileNames =  { Files.name };
+
+        tok = tok+6;
+
+        % Then through individual Audio Files:
+        for fileNo = 1:size(FileNames,2)
+            i_file = path + FileNames(fileNo);
+            
+            %Load in Audio
+            aud = audioread(i_file,[1,600*16000]); % Load in full file 
+            
+    
+            % Subset audio just a minute at a time:
+            for endSamp = 960000:9600000:9600000 %temp: just do it once
+                startSamp = 1;
+                samples = [startSamp,endSamp];
                 
-                col = "#"+ colours2(k);
-                % Plot Spectrogram
-                subplot(2,3,j);
-                e=plot(pdb(:,1), pdb(:,2), 'color',col,'linewidth',1);
-                e.Color = [e.Color 0.25];
-                %title(ylab)
-                hold on
+                TestSection = aud(startSamp:endSamp,:);
+                startSamp = endSamp; % Swap over the start/end point for next loop
+                
+                % Iterate through channels
+    
+                j=0 + tok; % This will be the figure number!
+    
+                for ch = 1:6
+    
+                    j=j+1;
+    
+                    audCh = aud(:,ch);
+                    [p,frequencies] = pspectrum(audCh,16000);
+                    pdb = cat(2,frequencies,pow2db(p));
+    
+                    ylab = "ch " +  j;
+                   
+                    % Get Colour
+                    col = "#"+ colours(k);
+    
+                    % Plot Spectrogram
+                    subplot(8,6,j);
+                    e=plot(pdb(:,1), pdb(:,2), 'color',col,'linewidth',1);
+                    e.Color = [e.Color 0.25];
+                    set(gca,'Yticklabel',[]) 
+                    set(gca,'Xticklabel',[])
+
+                    % Just Title Top Row
+                    if j <= 6
+                        title(ylab)
+                    end
+                    hold on
+                end 
             end 
+            disp(i_file + " done!")
         end 
-        disp(i_file + " done!")
     end
-        saveas(gcf, figName)
-        close
 end
 
 
