493038ec284695f16cf2b08fb324294399756d01
BeckyHeath <r.heath18@imperial.ac.uk>
2022-08-14 13:41:28 +0100

    generate just sweep + tone spectra

--- test_signal_comparisson.m
+++ test_signal_comparisson.m
@@ -11,28 +11,30 @@ tmp = matlab.desktop.editor.getActive;
 cd(fileparts(tmp.Filename));
 
 % Set up File Locations and Load in Relevant Data
-audio_path = "Data/Lab_Localisation/Audio_Data_Edited/Files_clean3/";
+audio_path = "Data/CompleteLabLocalisation/Test_Signals/";
 
-desc1 = "Rec2";
-desc2 = "Rec3";
-fn_1 = "5a_pinknoise_Y_2";
-fn_2 = "3a_pinknoise_Y_3";
+desc1 = "Pink Noise";
+desc2 = "Bird Song";
+fn_1 = "pink_noise";
+fn_2 = "Eurasian_Wren_";
 
 % Load in Audio
-samples = [1,1440000]; % whole signal
+samples = [1,1100000]; % whole signal
 aud1 = audioread(audio_path+fn_1+".wav",samples);
 aud2 = audioread(audio_path+fn_2+".wav",samples);
 
 
 %PLOT SPECTROGRAMS
 % Generate Spectrogram
+
+
 subplot(2,2,[1,2]);
-spectrogram(aud1,1300,1200,1300,48000,'yaxis')
+spectrogram(aud1,5280,1200,12800,48000,'yaxis')
 title(desc1)
 caxis([-110 -30])
 
 subplot(2,2,[3,4]);
-spectrogram(aud2,1280,1200,1280,48000,'yaxis')
+spectrogram(aud2,5280,1200,12800,48000,'yaxis')
 title(desc2)
 caxis([-110 -30])
 
