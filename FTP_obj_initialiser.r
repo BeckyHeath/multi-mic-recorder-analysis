a96cefbe38b26bb1a65c6b72a18b764f0d3048ac
BeckyHeath <r.heath18@imperial.ac.uk>
2022-12-14 17:32:50 +0000

    Create and File template for creating an FTP Object

---
+++ FTP_obj_initialiser.r
@@ -0,0 +1,13 @@
+########################################################################### .
+# Scripts to count file numbers by folder on a remote repo
+# CHANGE CREDENTIALS TO YOUR OWN
+#
+# Becky Heath
+# r.heath18@imperial.ac.uk
+#
+# Winter 2021
+
+##### Load Packages and set working directory #####
+
+library(boxr)
+box_auth(client_id = "yourUname" , client_secret = "YourPassword")
