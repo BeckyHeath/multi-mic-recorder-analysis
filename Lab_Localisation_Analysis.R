e734dc7548d1b6fcabd3a6afec7d82a4b9d0ef61
BeckyHeath <r.heath18@imperial.ac.uk>
2023-05-26 14:48:08 +0100

    tidy the localisation figure grouped by test group

--- Lab_Localisation_Analysis.R
+++ Lab_Localisation_Analysis.R
@@ -1,152 +1,158 @@
 ########################################################################### .
-# Scripts for Analysing the Sweeps 
+# Scripts for Analysing the Localisation Accuracy 
 #
 # Becky Heath
-# r.heath18@imperial.ac.uk
+# rh862@cam.ac.uk
 #
-# Autumn 2021
+# May 2023
 
 ##### Load Packages and set working directory #####
 
-library(stringr)
 library(ggplot2)
-library(plyr)
-library(patchwork)
+library(dplyr)
+
+
+
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 
-##### Define Test File Location #####
-
-file_directory = "Data/Lab_Localisation/dev2_clean_outputs/"
-
-
-##### Define Functions #####
+###### Load in Data ########
 
 # Load in true values: 
-true <- read.csv("Data/Real_Location_Clean.csv")
+data <- read.csv("Data/CompleteLabLocalisation/AllTests_LocalisationData.csv")
 
-section_data <- function(df){
-  
-  # Function to group the real data into distinct time periods?? 
-  # The recordings are then in groups to better compare real/predicted
-  #
-  # recallibrate start position to -180 
-  # 30deg mismatch in the transfer function (See HARKTOOL5)
-  
-  df$Start.azimuth <- df$Start.azimuth - 30
-  
-  # Numbers not divisible by 15 will be Removed 
-  
-  df$Start.time[df$Start.time < 7] <- 1  # Sweep Signal (ignored)
-  df$Start.time[df$Start.time > 15 & df$Start.time < 20] <- 15  # First Tone  
-  df$Start.time[df$Start.time > 30 & df$Start.time < 35] <- 30  # Second Tone
-  df$Start.time[df$Start.time > 45 & df$Start.time < 50] <- 45  # Third Tone
-  df$Start.time[df$Start.time > 60 & df$Start.time < 65] <- 60  # Fourth Tone
-  df$Start.time[df$Start.time > 75 & df$Start.time < 80] <- 75  # Fifth Tone
+# For the analysis at the moment let's just use waterproofed devices
+data <- data[data$wpREVERSED == "y",]
 
-  return(df)
-}
+data <- data %>%
+  mutate(trial = case_when(
+    trial == "SilwoodField" ~ "Field",
+    trial == "IndoorPre" ~ "Lab",
+    # Add more conditions if needed
+    TRUE ~ trial  # Keep the original value if no condition matches
+  ))
+
+data <- data %>%
+  mutate(label = paste0(trial, "_", distance.m, "m_", substr(tone, 1, 1)))
+
+# NOTES - This intentionally ignores weatherproofing! If you want to look at the weatherproofing you'll need
+#         to edit this code. 
 
 
-true_pred_plots <- function(df,tag,label){
-  
-  # Creates plots showing true vs. predicted values
-  
-  # Merge Data 
-  df <- df[, c("Start.time", "Start.azimuth")]
-  df <- rename(df, c("Start.azimuth" = "Predicted.Azimuth"))
-  merge_df <- merge(df, true, by=c("Start.time"),all=TRUE)
-  merge_df <- rename(merge_df, c("Start.azimuth" = "Real.Azimuth"))
-  merge_df_er <- merge_df[complete.cases(merge_df),]
-  
-  dif <- merge_df_er$Real.Azimuth - merge_df_er$Predicted.Azimuth
-  difLabel = paste("dif_",label, sep="")
-  assign(difLabel, dif, envir = .GlobalEnv)
-  
-  error_data <- find_error(merge_df_er$Real.Azimuth, merge_df_er$Predicted.Azimuth)
-  printout <- paste0("\n###############\nFile:", label, "\nMean Error:", error_data[1],"\nSD:", error_data[2], "\nR_sq:", error_data[3])
-  cat(printout)
-  
-  plot <- ggplot(merge_df, aes(Real.Azimuth, Predicted.Azimuth))+
-    ggtitle(label_g) +
-    geom_point(color = "Red", size =4, shape = 4, stroke = 1.5)+
-    geom_abline(color= "black", size = 0.6, alpha = 0.5) +
-    xlim(-180,180)+
-    ylim(-180,180)+
-    xlab("Predicted") + 
-    ylab("True") +
-    annotate("text", x = -150, y = 150, label = tag) +
-    theme_minimal()
-  
-  return(plot)
-}
 
-
-find_error <- function(x,y){
-  # finds the mean and sd of the error and r-sq where:
-  # x = real 
-  # and y = predicted
-  dif <- y-x
-  dif = dif * dif 
-  dif = sqrt(dif)
-  mean_val <- mean(dif)
-  sd_val <- sd(dif)
-  r_sq <- 1-sum((x-y)^2)/sum((x - mean(x))^2)
-  out <- c(mean_val,sd_val,r_sq)
-  return(out)
-}
-
-
-##### Generate Real vs. Predicted Plots ####
-
-j=0
-# Load in all the files you need: 
-for(i in list.dirs(file_directory, recursive = FALSE)){
-  path = paste(as.character(i),"sourcelist.csv", sep = "/")
-  i_file = read.csv(path, sep = "\t")
-  
-  # Generate Label
-  
-  # tidy label/ graph title name: 
-  label = str_remove(as.character(i), file_directory)
-  label = str_remove(label,"/localized_")
-  label = str_remove(label,".wav")
-  j=j+1
-  
-  
-  # Set Graph Labels (Tags)
-  tags = array(data=c('A','C','B','D'))
-  tag = tags[j]
-  
-  print(paste0("J = ",j))
-  
-  if(j == 1){
-    label_g = "Not Weatherproofed"
-  } else if(j==3){
-    label_g = "Weatherproofed"
-  } else {
-    label_g =""
-  }
-  
-  # Stop files that didn't catch any signals tripping out the loop
-  if(nrow(i_file) == 0){
-    next
-  }
-  
-  i_file <- section_data(i_file)
-  o_plot <- true_pred_plots(i_file,tag,label)
-  
-  assign(label, o_plot)
-}
+# Create Coarse Labells (no waterproofing)
 
 
 
 
-# Patchwork Plots 
-plot <- `1a_pinknoise_N_2`| `5a_pinknoise_Y_2`
-plot2 <- `1b_bird_N_2` | `5b_bird_Y_2`
 
-plot/plot2
+# Starting from scratch - go to the retired code if you want to generate the old figures
 
 
+# Calculate median and IQR per group
+data$distance.m <- as.factor(data$distance.m)
+data$difference <- as.numeric(data$difference)
 
+medianIQRs <- data %>%
+  group_by(distance.m) %>%
+  summarise(
+    Median = median(difference, na.rm= TRUE),
+    Q1 = quantile(difference, 0.25, na.rm= TRUE),
+    Q3 = quantile(difference, 0.75, na.rm = TRUE),
+    IQR = Q3 - Q1,
+    .groups = "keep"
+  )
+
+# ALL DATA (INSIDE AND OUT)
+ggplot(medianIQRs, aes(x = distance.m, y = Median, fill = distance.m)) +
+  geom_boxplot(width = 0.6) +
+  geom_errorbar(aes(ymin = Q1, ymax = Q3), width = 0.2) +
+  labs(title = "Grouped Boxplot with Median and IQR",
+       x = "Group", y = "Value") +
+  theme_minimal()
+
+
+# JUST SILWOOD DATA 
+field <- data[data$trial == "SilwoodField",]
+field$difference <- as.numeric(field$difference)
+field$distance.m <- as.factor(field$distance.m)
+
+# All Sounds
+medianIQRSilwood <- field %>%
+  group_by(distance.m) %>%
+  summarise(
+    Median = median(difference, na.rm= TRUE),
+    Q1 = quantile(difference, 0.25, na.rm= TRUE),
+    Q3 = quantile(difference, 0.75, na.rm = TRUE),
+    IQR = Q3 - Q1,
+    .groups = "keep"
+  )
+
+ggplot(medianIQRSilwood, aes(x = distance.m, y = Median, fill = distance.m)) +
+  geom_boxplot(width = 0.6) +
+  geom_errorbar(aes(ymin = Q1, ymax = Q3), width = 0.2) +
+  labs(title = "Grouped Boxplot with Median and IQR",
+       x = "Group", y = "Value") +
+  theme_minimal()
+
+# Separated By Sounds
+medianIQRSilwoodSep <- field %>%
+  mutate(distance.m = factor(distance.m)) %>%
+  group_by(distance.m, tone) %>%
+  summarise(
+    Median = median(difference, na.rm = TRUE),
+    Q1 = quantile(difference, 0.25, na.rm = TRUE),
+    Q3 = quantile(difference, 0.75, na.rm = TRUE),
+    IQR = Q3 - Q1,
+    .groups = "keep"
+  )
+
+
+ggplot(medianIQRSilwoodSep, aes(x = distance.m, y = Median, fill = tone, colour = tone)) +
+  geom_boxplot(width = 0.5, position = position_dodge(width = 0.7)) +
+  geom_errorbar(aes(ymin = Q1, ymax = Q3, colour = tone), width = 0.2, position = position_dodge(width = 0.7)) +
+  ylim(-15,15)+
+  labs(title = "Grouped Boxplot with Median and IQR", x = "Group", y = expression("Median Angle Difference "~(degree))) +
+  theme_minimal()
+
+
+
+
+
+plot1 <- ggplot(field, aes(x=distance.m, y=difference, fill = distance.m))+
+  geom_hline(yintercept=0)+
+  geom_violin(width = 0.8)+
+  theme_minimal()+
+  ylim(-100,100)
+  
+
+plot1
+
+###  FROM CHAT GTP: 
+library(dplyr)
+
+
+
+
+##########
+
+lab <- data[data$trial == "IndoorPre",]
+lab$difference <- as.numeric(lab$difference)
+lab$tone <- as.character(lab$tone)
+
+plot1 <- ggplot(lab, aes(x=tone, y=difference, fill = tone))+
+  geom_hline(yintercept=0)+
+  geom_violin(width = 0.8)+
+  theme_minimal()+
+  ylim(-180,180)
+
+
+plot1
+
+
+
+
+
+
+
+# Example dataframe with groups and NA value
