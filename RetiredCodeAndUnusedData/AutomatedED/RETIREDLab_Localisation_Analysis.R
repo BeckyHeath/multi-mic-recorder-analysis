ebcebe39c427c26d945274b89289c319718e0f6a
BeckyHeath <r.heath18@imperial.ac.uk>
2023-05-26 14:46:10 +0100

    Tidy repo

---
+++ RetiredCodeAndUnusedData/AutomatedED/RETIREDLab_Localisation_Analysis.R
@@ -0,0 +1,152 @@
+########################################################################### .
+# Scripts for Analysing the Sweeps 
+#
+# Becky Heath
+# r.heath18@imperial.ac.uk
+#
+# Autumn 2021
+
+##### Load Packages and set working directory #####
+
+library(stringr)
+library(ggplot2)
+library(plyr)
+library(patchwork)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+##### Define Test File Location #####
+
+file_directory = "Data/Lab_Localisation/dev2_clean_outputs/"
+
+
+##### Define Functions #####
+
+# Load in true values: 
+true <- read.csv("Data/Real_Location_Clean.csv")
+
+section_data <- function(df){
+  
+  # Function to group the real data into distinct time periods?? 
+  # The recordings are then in groups to better compare real/predicted
+  #
+  # recallibrate start position to -180 
+  # 30deg mismatch in the transfer function (See HARKTOOL5)
+  
+  df$Start.azimuth <- df$Start.azimuth - 30
+  
+  # Numbers not divisible by 15 will be Removed 
+  
+  df$Start.time[df$Start.time < 7] <- 1  # Sweep Signal (ignored)
+  df$Start.time[df$Start.time > 15 & df$Start.time < 20] <- 15  # First Tone  
+  df$Start.time[df$Start.time > 30 & df$Start.time < 35] <- 30  # Second Tone
+  df$Start.time[df$Start.time > 45 & df$Start.time < 50] <- 45  # Third Tone
+  df$Start.time[df$Start.time > 60 & df$Start.time < 65] <- 60  # Fourth Tone
+  df$Start.time[df$Start.time > 75 & df$Start.time < 80] <- 75  # Fifth Tone
+
+  return(df)
+}
+
+
+true_pred_plots <- function(df,tag,label){
+  
+  # Creates plots showing true vs. predicted values
+  
+  # Merge Data 
+  df <- df[, c("Start.time", "Start.azimuth")]
+  df <- rename(df, c("Start.azimuth" = "Predicted.Azimuth"))
+  merge_df <- merge(df, true, by=c("Start.time"),all=TRUE)
+  merge_df <- rename(merge_df, c("Start.azimuth" = "Real.Azimuth"))
+  merge_df_er <- merge_df[complete.cases(merge_df),]
+  
+  dif <- merge_df_er$Real.Azimuth - merge_df_er$Predicted.Azimuth
+  difLabel = paste("dif_",label, sep="")
+  assign(difLabel, dif, envir = .GlobalEnv)
+  
+  error_data <- find_error(merge_df_er$Real.Azimuth, merge_df_er$Predicted.Azimuth)
+  printout <- paste0("\n###############\nFile:", label, "\nMean Error:", error_data[1],"\nSD:", error_data[2], "\nR_sq:", error_data[3])
+  cat(printout)
+  
+  plot <- ggplot(merge_df, aes(Real.Azimuth, Predicted.Azimuth))+
+    ggtitle(label_g) +
+    geom_point(color = "Red", size =4, shape = 4, stroke = 1.5)+
+    geom_abline(color= "black", size = 0.6, alpha = 0.5) +
+    xlim(-180,180)+
+    ylim(-180,180)+
+    xlab("Predicted") + 
+    ylab("True") +
+    annotate("text", x = -150, y = 150, label = tag) +
+    theme_minimal()
+  
+  return(plot)
+}
+
+
+find_error <- function(x,y){
+  # finds the mean and sd of the error and r-sq where:
+  # x = real 
+  # and y = predicted
+  dif <- y-x
+  dif = dif * dif 
+  dif = sqrt(dif)
+  mean_val <- mean(dif)
+  sd_val <- sd(dif)
+  r_sq <- 1-sum((x-y)^2)/sum((x - mean(x))^2)
+  out <- c(mean_val,sd_val,r_sq)
+  return(out)
+}
+
+
+##### Generate Real vs. Predicted Plots ####
+
+j=0
+# Load in all the files you need: 
+for(i in list.dirs(file_directory, recursive = FALSE)){
+  path = paste(as.character(i),"sourcelist.csv", sep = "/")
+  i_file = read.csv(path, sep = "\t")
+  
+  # Generate Label
+  
+  # tidy label/ graph title name: 
+  label = str_remove(as.character(i), file_directory)
+  label = str_remove(label,"/localized_")
+  label = str_remove(label,".wav")
+  j=j+1
+  
+  
+  # Set Graph Labels (Tags)
+  tags = array(data=c('A','C','B','D'))
+  tag = tags[j]
+  
+  print(paste0("J = ",j))
+  
+  if(j == 1){
+    label_g = "Not Weatherproofed"
+  } else if(j==3){
+    label_g = "Weatherproofed"
+  } else {
+    label_g =""
+  }
+  
+  # Stop files that didn't catch any signals tripping out the loop
+  if(nrow(i_file) == 0){
+    next
+  }
+  
+  i_file <- section_data(i_file)
+  o_plot <- true_pred_plots(i_file,tag,label)
+  
+  assign(label, o_plot)
+}
+
+
+
+
+# Patchwork Plots 
+plot <- `1a_pinknoise_N_2`| `5a_pinknoise_Y_2`
+plot2 <- `1b_bird_N_2` | `5b_bird_Y_2`
+
+plot/plot2
+
+
+
---
+++ RetiredCodeAndUnusedData/GainAdjustmentStuff/deadSigDetection.m
@@ -0,0 +1,77 @@
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Here we're looking to detect gain/ dead channel issues!
+% Adapted from Field + Lab test scipts for general use
+%
+%
+% Becky Heath with advice from Dan Harmer + Lorenzo Picinali
+% Summer 2022
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Set working directory 
+tmp = matlab.desktop.editor.getActive;
+cd(fileparts(tmp.Filename));
+
+
+% Set audio and output locations 
+audio_dir_path = 'Data\WavFieldRecordings\Raw'; % path to audio 
+outFileRoot = "Data\WavFieldRecordings\RawAbsMaxVals"; % include file name, but DO NOT include ending (.csv)
+
+
+recLength = 75; % Recording Length (seconds)
+recFreq = 16000; % Recording Frequency (Hz)
+
+% Setup Output DFs: 
+outMat = ["fileName","ch1","ch2","ch3","ch4","ch5","ch6"];
+
+outDesc = ["fileName","ch1","ch2","ch3","ch4","ch5","ch6"];
+
+      
+% Get Filenames
+file_names = getfn(audio_dir_path, ".wav");
+
+% Load in data and join all sprectral data together
+for i = 1:size(file_names,2)
+
+    %Load in File
+    fileName = string(file_names{i});
+    filePath = fileName; 
+
+    samples = [1,recLength*recFreq]; 
+    aud = audioread(filePath,samples);
+    
+    % Use the absolute of the mean values to 
+    % get a metric of the status of each channel
+    outRaw= max(abs(aud));
+      
+    % consolidate this data:     
+    outLine = [filePath,outRaw]; % add file label
+    outMat = [outMat;outLine];
+
+
+    % Get Text Descriptions here (Edit if necessary)
+    outRaw = string(outRaw);
+    for j = 1:size(outRaw,2)
+        val = str2double(outRaw(j));
+        if val >= 0.5
+            outRaw(j) = "OverPower";
+        end
+        if val < 0.5
+            outRaw(j) = "ok";
+        end
+        if val <= 0.01
+            outRaw(j) = "dead";
+        end
+    end
+    
+    % Consolidate info: 
+    outRaw = [filePath,outRaw]; % Append file name
+    outDesc = [outDesc;outRaw];
+    
+end      
+
+rawFileName = outFileRoot + ".csv";
+descFileName = outFileRoot + "_Desc.csv";
+writematrix(outMat,rawFileName);
+writematrix(outDesc,descFileName);
+
+fclose('all');
