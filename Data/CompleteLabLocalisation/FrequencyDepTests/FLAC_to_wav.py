5a1a620ef4abd8c6f2d43ff2a11506f22ee6868f
BeckyHeath <r.heath18@imperial.ac.uk>
2024-03-17 08:48:36 +0000

    Starting the 48kHz experiments

---
+++ Data/CompleteLabLocalisation/FrequencyDepTests/FLAC_to_wav.py
@@ -0,0 +1,23 @@
+################################################################################################
+#
+# Script to Convert FLAC files to WAV files for use with HARKBird
+# 
+# Becky Heath 2021
+#
+################################################################################################
+
+# Imports
+import os
+import subprocess
+                    #Move To Folder with Audio Files in 
+
+pre_comp_dir = "16kHz-Recording-new/"
+post_comp_dir ="16kHz-Recording-new/"
+
+for wfile in os.listdir(pre_comp_dir):
+    wfile = pre_comp_dir + wfile
+    ofile= wfile.replace(pre_comp_dir, post_comp_dir)
+    ofile = ofile.replace(".wav", ".flac")
+    cmd = ('ffmpeg -i {} {}') 
+    subprocess.call(cmd.format(wfile, ofile), shell=True)
+    #os.remove(wfile)
